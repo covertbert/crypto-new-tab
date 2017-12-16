6e84bb36a6e97b6ddc012b450d9fca771bdffdb0
Bertie Blackman <blackmanrgh@gmail.com>
2017-12-16 22:51:03 +0000

    Ticker component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<template lang="pug">
+	#app
+		ticker-section
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import TickerSection from './components/ticker/TickerSection.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      TickerSection
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" rel="stylesheet/scss">
+	html {
+		font-size: 16px;
+
+		@media screen and (min-width: 900px) {
+			font-size: 18px;
+		}
+
+		@media screen and (min-width: 1200px) {
+			font-size: 20px;
+		}
+	}
+
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
---
+++ src/components/ticker/TickerSection.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+	section.container.ticker-section
+		ticker-component
+</template>
+
+<script type="text/babel">
+  import axios from 'axios'
+  import TickerComponent from './TickerComponent.vue'
+
+  export default {
+    name: 'TickerSection',
+
+    extends: {},
+
+    props: {},
+
+    data () {
+      return {
+        price: null
+      }
+    },
+
+    computed: {},
+
+    components: {TickerComponent},
+
+    watch: {},
+
+    methods: {},
+
+    beforeCreate () {
+    },
+
+    mounted () {
+      axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+        .then((response) => {
+          this.price = response.data.ticker.price
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+	.ticker-section {
+		display: flex;
+	}
+</style>
